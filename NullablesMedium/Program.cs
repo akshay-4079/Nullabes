37b739f13c1b74f61ae229d237bef2d4628e07b0
Akshay <akshaysrikumar@gmail.com>
2019-02-27 07:44:28 -0600

    C#

---
+++ NullablesMedium/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CalculatorApplication
+{
+    class Nullables
+    {
+        static void Main(string[] args)
+        {
+            NullableCompare();
+            Helper();
+
+        }
+
+        private static void Helper()
+        {
+            int? i = null;
+            int j = 10;
+
+            if (Nullable.Compare<int>(i, j) < 0)
+                Console.WriteLine("i < j");
+            else if (Nullable.Compare<int>(i, j) > 0)
+                Console.WriteLine("i > j");
+            else
+                Console.WriteLine("i = j");
+        }
+
+        private static void NullableCompare()
+        {
+            int? i = null;
+            int j = 10;
+
+
+            if (i < j)
+                Console.WriteLine("i < j");
+            else if (i > 10)
+                Console.WriteLine("i > j");
+            else if (i == 10)
+                Console.WriteLine("i == j");
+            else
+                Console.WriteLine("Could not compare");
+        }
+    }
+}
